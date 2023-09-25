223fa487a95750806f2ef065bbd519fc893d9050
laura1513 <laurajorques2003@gmail.com>
2023-09-25 13:47:08 +0200

    adelantar

--- Libros/Books.java
+++ Libros/Books.java
@@ -1,2 +1,38 @@
 public class Books {
+    String titulo;
+    String autor;
+    int numeroPaginas;
+
+    public Books(String titulo, String autor, int numeroPaginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public Books() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroPaginas;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
+    }
 }
--- Libros/Main.java
+++ Libros/Main.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Books> libros = new ArrayList<>();
+        libros.add(new Books("Harry Potter", "J.K. Rowling", 470));
+        libros.add(new Books("Los tres cerditos", "Paco Paquito", 30));
+        libros.add(new Books("Don Quijote", "Cervantes", 340));
+        libros.add(new Books("El señor de los anillos", "J.R.R. Tolkien", 576));
+
+        /*long cont1 = libros.stream().filter(l -> l.getNumeroPaginas() > 500).count();
+        System.out.println("Hay " + cont1 + " libros con más de 500 páginas");*/
+        int cont1 = 0;
+        for (Books b : libros) {
+            if (b.getNumeroPaginas() >= 500) {
+                cont1++;
+            }
+        }
+        System.out.println("Hay " + cont1 + " libro(s) con más de 500 páginas");
+
+        /*long cont2 = libros.stream().filter(l -> l.getNumeroPaginas() < 300).count();
+        System.out.println("Hay " + cont2 + " libros con menos de 300 páginas");*/
+        int cont2 = 0;
+        for (Books b : libros) {
+            if (b.getNumeroPaginas() <= 300) {
+                cont2++;
+            }
+        }
+        System.out.println("Hay " + cont2 + " libro(s) con menos de 300 páginas");
+
+        for (Books b : libros) {
+            if (b.getNumeroPaginas() >= 500) {
+                System.out.println(b.getTitulo() + " tiene más de 500 páginas");
+            }
+        }
+
+        
+    }
 }
